387173b3203c5546c626c191478741b6911bca37
1norahc <76412079+1norahc@users.noreply.github.com>
2022-07-26 12:25:39 +0200

    Update linear_regression.py

--- learning/py-math/Linear Regression/linear_regression.py
+++ learning/py-math/Linear Regression/linear_regression.py
@@ -3,55 +3,18 @@ import matplotlib.pyplot as plt
 
 
 
-x = np.array([0.1,0.4,0.30,0.45,0.5,0.55,0.90])
-y =          [0.1,0.2,0.3 ,0.4 ,0.5,0.6 ,0.7]
+def test_values(sample_range: int, dfY=True):
+    _dfY = []
+    _ = 0.1
+    for i in range(0, sample_range-1):
+        _+=0.1
+        _dfY.append(_)
+    #_dfY[-1] = 1.0
+    random_sample = [np.random.random_sample(2) for _ in range(0, sample_range-1)]
+    if dfY != True:
+        X, Y = [v[0] for v in random_sample], [v[1] for v in random_sample]
+    else:
+        X, Y = [v[0] for v in random_sample], _dfY
+    return X, Y
 
-#print(x)
-#print(y)
-
-plt.scatter(y, x, c='red')
-
-wagi=0
-bias=0
-
-def koszt(wagi, bias, x, wartos_prawdziwa):
-	predykcja = x*wagi+bias # y=wx+b
-	MSE  = (1/len(y)) * np.sum(np.square(wartos_prawdziwa-predykcja))
-	return MSE
-
-def spadek_gradientu(x, y, lr, iteracje):
-    bias = 0
-    wagi = 0.1 
-    
-    for n in range(iteracje):
-        predykcja = x * wagi + bias  
-
-        dw = 1/len(x) * np.sum((-2*x*(predykcja - y)))
-        db = 1/len(x) * np.sum(2*(predykcja - y))
-        
-        wagi = wagi - lr * dw
-        bias = bias - lr * db
-        
-        if n % 30 == 0:
-            print("Loss : ",koszt(wagi,bias,x,y))
-            
-    return wagi, bias  
-wagi,bias = spadek_gradientu(x,y,0.2,150)
-predykcja = x * wagi + bias         
-plt.scatter(y,x,c="red")
-plt.plot(predykcja,x,c="green")
-
-
-from IPython.display import clear_output
-import time 
-
-for i in range(0,150):
-    plt.figure()
-    wagi,bias = spadek_gradientu(x,y,0.2,i)
-    predykcja = x * wagi + bias         
-    plt.scatter(y,x,c="red")
-    plt.plot(predykcja,x,c="green")
-    plt.show()
-    time.sleep(0.15)
-    
-    clear_output(wait=True)
+print(test_values(10))
